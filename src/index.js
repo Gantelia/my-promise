6ef337ab177e52f0168703996e4738d2b11f1e9f
Gantelia <helengriva@gmail.com>
2022-05-31 21:29:23 +0300

    Добавит метод then, его проверку и описание. Установит eslint для удобства работы

--- src/index.js
+++ src/index.js
@@ -1,53 +1,89 @@
 const PromiseState = {
-    PENDING: 'pending',
-    REJECTED: 'rejected',
-    FULFILLED: 'fulfilled',
-}
+  PENDING: 'pending',
+  REJECTED: 'rejected',
+  FULFILLED: 'fulfilled',
+};
 
 class MyPromise {
-    constructor(executor) {
-        this.state = PromiseState.PENDING;
-        this.result = undefined;
+  constructor(executor) {
+    this.state = PromiseState.PENDING;
+    this.result = undefined;
+    this.onFulfilledFn = [];
+    this.onRejectedFn = [];
 
-        const resolve = (value) => {
-            if (this.state === PromiseState.PENDING) {
-                this.state = PromiseState.FULFILLED;
-                this.result = value;
-                console.log(3, value);
-            }
-        };
+    const resolve = (value) => {
+      if (this.state === PromiseState.PENDING) {
+        this.state = PromiseState.FULFILLED;
+        this.result = value;
+        console.log(3, value);
+        this.onFulfilledFn.forEach((fn) => fn(value));
+      }
+    };
 
-        const reject = (error) => {
-            if (this.state === PromiseState.PENDING) {
-                this.state = PromiseState.REJECTED;
-                this.result = error;
-            }
-        };
+    const reject = (error) => {
+      if (this.state === PromiseState.PENDING) {
+        this.state = PromiseState.REJECTED;
+        this.result = error;
+        this.onRejectedFn.forEach((fn) => fn(error));
+      }
+    };
 
-        try {
-            executor(resolve, reject);
-        } catch (error) {
-            reject(error);
-        }
-    } 
+    try {
+      executor(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
+  }
+
+  then = (onFulfilled, onRejected) => {
+    if (this.state === PromiseState.PENDING) {
+      if (onFulfilled) {
+        this.onFulfilledFn.push(onFulfilled);
+        return;
+      }
+      if (onRejected) {
+        this.onRejectedFn.push(onRejected);
+        return;
+      }
+    }
+
+    if (onFulfilled && this.state === PromiseState.FULFILLED) {
+      onFulfilled(this.result);
+      return;
+    }
+
+    if (onRejected && this.state === PromiseState.REJECTED) {
+      onRejected(this.error);
+    }
+  };
 }
 
-console.log('FIRST PROMISE INSTANCE')
+console.log('FIRST PROMISE INSTANCE');
 console.log(1);
-const promise = new MyPromise((resolve, reject) => {
-    console.log(2);
-    setTimeout(() => console.log('Executes after all syncronous code:', 5), 0);
-    resolve('success');
-    resolve('unexecuted code');
+const firstPromise = new MyPromise((resolve, reject) => {
+  console.log(2);
+  setTimeout(() => console.log('Executes after all syncronous code:', 5), 0);
+  resolve('success');
+  resolve('unexecuted code');
 });
 console.log(4);
 
 
+const secondPromise = new MyPromise((resolve, reject) => {
+  console.log('SECOND PROMISE INSTANCE');
+  setTimeout(() => reject('error'), 1000);
+});
 
-const rejectedPromise = new MyPromise((resolve, reject) => {
-    console.log('SECOND PROMISE INSTANCE')
-    setTimeout(() => reject('error'), 1000);
-})
+setTimeout(
+    () => console.log('Asyncronous 2-nd promise check:', secondPromise),
+    1500,
+);
+console.log('Syncronous 2-nd promise check:', secondPromise);
 
-setTimeout(() => console.log('Asyncronous promise check:', rejectedPromise), 2000)
-console.log('Syncronous promise check:', rejectedPromise);
+
+const thirdPromise = new MyPromise((resolve, reject) => {
+  console.log('THIRD PROMISE INSTANCE');
+  setTimeout(() => resolve('success to then'), 2000);
+}).then((value) => {
+  console.log(value);
+});
